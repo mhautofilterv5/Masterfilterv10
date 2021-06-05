453eb52d68e986157fc1a8f06effe364ffc7e958
mhautofilterv5 <85279476+mhautofilterv5@users.noreply.github.com>
2021-06-06 00:56:40 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -4,58 +4,29 @@
 
 class Translation(object):
     
-    START_TEXT = """<b>🙋‍♂️Hey {}!!</b>
+    START_TEXT = """<b>🙋‍♂️Hey {} What's Up Mahn 😎!!</b>
 
-<b>Am Just A Pro Auto Filter Bot V2....😉</b>
+<b>Am A Pro Auto Filter Bot V2 for MALLUZHUB....😈</b>
 
-<b>Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝</b>
+<b>You Can't Add Me To Your Group And Channel🥱🥱</b>
 
-<b>Watch the video below to build an Pro Autofilter bot V2 like me...</b>
-
-<b>Subscribe to the update channel to learn about my updates and activity...</b>
-
-<b>Press /help to know about available commands🤪</b>"""    
+<b>Press /help to know about available commands😌</b>"""    
     
     HELP_TEXT = """
-<b>😉How To Use Me!😉?</b>
+<b>🥱എന്തുവാ ഇത് !😈?</b>
 
-<i>
--> Add Me To Any Group And Make Me Admin
--> Add Me To Your Desired Channel
-</i>
-
-<b>Bot Commands (Works Only In Groups) :</b>
-
-    -> <code>/add chat_id</code>
-                OR                  - <b>To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)</b>
-     <code>/add @Username</code>
-     
-    -> <code>/del chat_id</code>
-                OR                  - <b>To disconnect A Group With A Channel</b>
-     <code>/del @Username</code>
-     
-    -> <code>/delall</code>  - <b>This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB</b>
-    
-    -> <code>/settings</code> -  <b>This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly</b>
-
-            -> <code>Channel</code> - <b>Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls</b>
-            
-            -> <code>Filter Types</code> - <b>Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart</b>
-
-            -> <code>Configure</code> - <b>Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results</b>
-            
-            -> <code>Status</code> - <b>Button Will Shows The Stats Of Your Channel</b>
-            
-<b><a href="https://t.me/Mo_Tech_YT">© Mo Tech YT</a></b>
+<b><n><i>ഈ ബോട്ട് ©Mallu Hub Movies ഗ്രൂപ്പിനു വേണ്ടി നിർമിച്ചതാണ്.Start കൊടുത്തു വെറുതെ സമയം പാഴാക്കല്ലേ സുഹൃത്തേ..! 
+നിങ്ങൾ പാഴക്കികളയുന്ന ഓരോ ടൈമും വിലപ്പെട്ടതാണ്..😌</i></n></b>
+<b><a href="https://t.me/MALLUZHUB">© malluhubbmovies</a></b>
 """
     
-    ABOUT_TEXT = """<b>➥ 📃Name</b> : <b>Pro Auto Filter Bot</b>
+    ABOUT_TEXT = """<b>➥ 📃Name</b> : <b>Malluzhub Pro Auto Filter Bot</b>
 
-<b>➥ 👨‍💻Editor</b> : <b><a href="https://t.me/MRk_YT">👤Mrk YT</a></b>
+<b>➥ 👨‍💻Editor</b> : <b><a href="https://t.me/MALLUZHUB">👤malluzteams</a></b>
 
 <b>➥ 🗣️Language</b> : <b>Python3<b>
 
 <b>➥ 📚Library</b> : <b><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></b>
 
-<b>➥ 📖Source Code</b> : <b><a href="https://github.com/MRK-YT/Pro-Auto-Filter-Bot-V2">💥Click Me</a></b>
+<b>➥ 📖Source Code</b> : <b><a href="https://github.com/mhautofilterv5/Autofiltermhv5">💥Click Me</a></b>
 """
