7122685f6fed97590d0c1cb8da4322776dc15733
mhautofilterv5 <85279476+mhautofilterv5@users.noreply.github.com>
2021-06-07 02:14:32 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -1682,8 +1682,8 @@ async def callback_data(bot, update: CallbackQuery):
 
     elif query_data == "about": 
         buttons = [[
-            InlineKeyboardButton('𝙼aster', url='https://t.me/MALLUZHUB')
-            InlineKeyboardButton('group', url='https://t.me/malluhubbmovies')
+            InlineKeyboardButton('🎭 @𝙼aster', url='https://t.me/MALLUZHUB')
+            InlineKeyboardButton('@group 🎭', url='https://t.me/malluhubbmovies')
         ],[
             InlineKeyboardButton('🍁 main man 😈', url='https://t.me/vaathi_comin_g'),
         ],[
