d92c88a31dce4ef84803b0aff671bbc7118eec4f
mhautofilterv5 <85279476+mhautofilterv5@users.noreply.github.com>
2021-06-07 01:20:17 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -53,7 +53,7 @@ async def start(bot, update):
             await bot.send_document(
                 chat_id=update.chat.id,
                 document = file_id,
-                caption = f"<b>{caption} \n @MALLUZHUB</b>",
+                caption = caption,
                 parse_mode="html",
                 reply_to_message_id=update.message_id,
                 reply_markup=InlineKeyboardMarkup(
